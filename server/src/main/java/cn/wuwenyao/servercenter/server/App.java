ebbce6de56a701bbda794b9439be624af895f992
shiqiyue <835802084@qq.com>
2016-03-04 15:01:34 +0800

    sdadad

---
+++ server/src/main/java/cn/wuwenyao/servercenter/server/App.java
@@ -0,0 +1,13 @@
+package cn.wuwenyao.servercenter.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
